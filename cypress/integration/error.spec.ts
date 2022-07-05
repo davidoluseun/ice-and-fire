273ee2f064fb61bf7970a830945ef8524411b851
David Oluseun <a.davidoluseun@gmail.com>
2022-07-05 20:11:15 +0100

    test: should try again after network error for getting more more books

--- cypress/integration/error.spec.ts
+++ cypress/integration/error.spec.ts
@@ -27,5 +27,31 @@ describe("App", () => {
       exec();
       cy.get('[data-testid="book"]').should("have.length", 6);
     });
+
+    it("should try again after network error for getting more more books", () => {
+      const url =
+        "https://www.anapioficeandfire.com/api/books?page=2&pageSize=6";
+
+      cy.intercept(url, { times: 1 }, { forceNetworkError: true }).as(
+        "getNetworkFailure"
+      );
+
+      cy.intercept(url).as("getBooks");
+
+      cy.visit("/");
+
+      exec();
+      cy.wait(5000);
+      cy.get('[data-testid="book"]').eq(5).scrollIntoView();
+      cy.get('[data-testid="next-spinner"]').should("exist");
+      cy.wait("@getNetworkFailure");
+      cy.get('[data-testid="next-spinner"]').should("not.exist");
+      cy.get('[data-testid="error"]').should("exist");
+      cy.get('[data-testid="error"]').contains("Try Again").click();
+      cy.get('[data-testid="next-spinner"]').should("exist");
+      cy.wait("@getBooks");
+      cy.get('[data-testid="next-spinner"]').should("not.exist");
+      cy.get('[data-testid="book"]').should("have.length", 12);
+    });
   });
 });
